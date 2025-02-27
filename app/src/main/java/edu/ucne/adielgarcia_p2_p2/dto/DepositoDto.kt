ee25a521f413d944f1d9f59ec93bc550d8e8ad22
Adiel040 <133602314+Adiel040@users.noreply.github.com>
2025-02-26 20:44:49 -0400

    Creando dto

---
+++ app/src/main/java/edu/ucne/adielgarcia_p2_p2/dto/DepositoDto.kt
@@ -0,0 +1,14 @@
+package edu.ucne.adielgarcia_p2_p2.dto
+
+class DepositoDto (
+
+
+
+        val idDeposito: Int? = null,
+        val fecha: String = "",
+        val idCuenta: Int,
+        val concepto: String = "",
+        val monto: Double = 0.0,
+
+
+        )
