d26ac4853df849b033bb777f81ad132c2048a89a
Adiel040 <133602314+Adiel040@users.noreply.github.com>
2025-02-28 00:56:39 -0400

    Personalizando Theme

---
+++ app/src/main/java/com/ucne/adielgarcia_p2_p2/ui/theme/Color.kt
@@ -0,0 +1,11 @@
+package com.ucne.adielgarcia_p2_p2.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Cyan80 = Color(0xFFBCFDFF)
+val CyanGrey80 = Color(0xFFC2CEDC)
+val Blue80 = Color(0xFFB8C8EF)
+
+val Cyan40 = Color(0xFF5097A4)
+val CyanGrey40 = Color(0xFF5B6B71)
+val Blue40 = Color(0xFF52597D)
